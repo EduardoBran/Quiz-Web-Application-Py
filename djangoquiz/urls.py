85663c7da851fae26709acd742a74dacc01a49cc
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-29 21:13:30 -0300

    criando a rota para a pagina inicial

--- djangoquiz/urls.py
+++ djangoquiz/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from quiz.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home')
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
