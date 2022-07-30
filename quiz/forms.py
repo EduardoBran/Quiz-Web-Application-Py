977d825bce527d661fe013f8abfad252ead8d146
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-30 13:07:58 -0300

    criado forms para criacao de usuario e adicionar pergunta

---
+++ quiz/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+from .models import *
+
+
+class createUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        
+
+class addQuestionForm(ModelForm):
+    class Meta:
+        model = QuestionsModel
+        fields = "__all__"
