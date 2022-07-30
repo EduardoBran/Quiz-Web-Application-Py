2a301adab0ada4e29dbee5311e5bab5c75b7a4c4
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-30 17:44:40 -0300

    criado nav | criado footer | criado templates e rotas

--- djangoquiz/urls.py
+++ djangoquiz/urls.py
@@ -13,16 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
 from quiz.views import *
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('addQuestion/', addQuestionPage, name='addQuestion'),
+    path('login/', loginPage, name='login'),
+    path('logout/', logoutPage, name='logout'),
+    path('register/', registerPage, name='register'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def home(request):
     return render(request, 'quiz/home.html')
+
+def addQuestionPage(request):
+    return render(request, 'quiz/addQuestion.html')
+
+def registerPage(request):
+    return render(request, 'quiz/register.html')
+
+def loginPage(request):
+    return render(request, 'quiz/login.html')
+
+def logoutPage(request):
+    return redirect('/')
