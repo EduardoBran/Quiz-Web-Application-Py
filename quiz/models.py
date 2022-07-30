da157d2ae78b58da5547e0ce6dd16aaf8ee89c3d
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-30 13:02:12 -0300

    criando models para pergunta , respostas e resposta correta

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class QuestionsModel(models.Model):
+    question = models.CharField(max_length=200, null=True)
+    op1 = models.CharField(max_length=200, null=True)
+    op2 = models.CharField(max_length=200, null=True)
+    op3 = models.CharField(max_length=200, null=True)
+    op4 = models.CharField(max_length=200, null=True)
+    ans = models.CharField(max_length=200, null=True)
