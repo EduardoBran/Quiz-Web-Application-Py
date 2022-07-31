ae34754672ab05383feceda6bb538cd5a3ffaff8
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-31 13:05:19 -0300

    editando models | editando admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,14 +3,37 @@ from django.contrib import admin
 from .models import *
 
 
+@admin.action(description='Mostrar todas as perguntas')
+def mostrar_todos(modeladmin, requeust, queryset):
+    queryset.update(mostrar=True)
+
+@admin.action(description='Não mostrar todas as perguntas')
+def nao_mostrar_todos(modeladmin, requeust, queryset):
+    queryset.update(mostrar=False)
+
 class QuestionsModelAdmin(admin.ModelAdmin):
     list_display = [
+        'q_number',
         'question',
-        'ans'
+        'ans',
+        'mostrar'
     ]
     
     search_fields = [
-        'question'
+        'question',   
+    ]
+    
+    list_editable = [
+        'mostrar'
+    ]
+    
+    list_filter = [
+        'mostrar'
+    ]
+    
+    actions = [
+        mostrar_todos,
+        nao_mostrar_todos
     ]
     
 
--- quiz/models.py
+++ quiz/models.py
@@ -8,3 +8,5 @@ class QuestionsModel(models.Model):
     op3 = models.CharField(max_length=200, null=True, verbose_name='Opção 3')
     op4 = models.CharField(max_length=200, null=True, verbose_name='Opção 4')
     ans = models.CharField(max_length=200, null=True, verbose_name='Resposta (Ex: "option1")')
+    q_number = models.BigIntegerField(unique=True, verbose_name='Questao Nº')
+    mostrar = models.BooleanField(default=True)
